0d2a897f2e4fa4c63594beb681fa9ae8f4d78deb
unknown <who.canada.syrup@gmail.com>
2015-10-31 22:26:30 -0400

    verifying some angular things work

--- controllers.js
+++ controllers.js
@@ -1,10 +1,57 @@
-var app = angular.module('portApp', ['ngAnimate', 'ui.bootstrap']);
-app.controller('moduleCtrl', function($scope, $uibModal) {
+var app = angular.module('portApp', ["ngAnimate", "ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+        templateUrl : 'intro.html',
+        controller : 'mainCtrl'
+    }) 
+        .when('/programming', {
+        templateUrl : 'programming.html',
+        controller : 'programmingCtrl'
+    })
+        .when('/artwork', {
+        templateUrl : 'art.html',
+        controller : 'artCtrl'
+    });
     
-    $scope.openModule = function() {
-        var infoModule = $uibModal.open({
-            animation: true;
-            templateUrl: 'contactModal.html'
-        });
+});
+
+app.controller('mainCtrl', function($scope) {
+    
+    
+    
+    $scope.hideIntro = false;
+    
+    $scope.art_to_intro = function() {
+        $scope.hideIntro = false;
+        $scope.hideProgramming = true;
+        $scope.hideArt = true;
+    };
+});
+
+app.controller('programmingCtrl', function($scope) {
+    
+    
+    
+    $scope.hideIntro = false;
+    
+    $scope.art_to_intro = function() {
+        $scope.hideIntro = false;
+        $scope.hideProgramming = true;
+        $scope.hideArt = true;
+    };
+});
+
+app.controller('artCtrl', function($scope) {
+    
+    
+    
+    $scope.hideIntro = false;
+    
+    $scope.art_to_intro = function() {
+        $scope.hideIntro = false;
+        $scope.hideProgramming = true;
+        $scope.hideArt = true;
     };
 });
